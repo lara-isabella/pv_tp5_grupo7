a1dfe51d7a9acc478de098af5b0834d8c4e5e34a
lara-isabella <laraisabellaregina@gmail.com>
2025-05-27 00:08:33 -0300

    Avances pagina de inicio - Lara Colazo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Inicio from "./assets/components/inicio"
 
 function App() {
 
   return (
-    <>
       <div>
-        
+        <Routes>
+        <Route path="/" element={ <Inicio /> } />
+        </Routes>
       </div>
       
-    </>
   )
 }
 
---
+++ src/assets/components/inicio.jsx
@@ -0,0 +1,10 @@
+function Inicio() {
+  return (
+    <div>
+      <h1>Esta es la página de inicio</h1>
+      <p>Bienvenido</p>
+    </div>
+  );
+}
+
+export default Inicio;
